e624be3aba0157973a60ed15d51fc07b4c5c0bae
iamlydial <lydia.lavecchia@gmail.com>
2023-10-21 22:16:39 +0200

    edits

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 HOST = 'localhost'
 USER = 'root'
-PASSWORD = 'smiley1987'
+PASSWORD = 'root database password'
 # 1. insert your db password
 # 2. go to git ignore and add uncomment the line on line 10
 # 3. upload add, commit the file to our git hub
