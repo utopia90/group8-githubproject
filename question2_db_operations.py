292f72803e48845e03412e7a75e6ccdd74655c09
iamlydial <lydia.lavecchia@gmail.com>
2023-10-23 15:33:27 +0200

    added 3: 1) retrieves the menu def get_menu(): 2) def get_alcoholic_ingredients(menu): 3)def get_cocktail_by_alcoholic_beverage(menu, user_selection):

--- question2_db_operations.py
+++ question2_db_operations.py
@@ -1,12 +1,14 @@
 #  SUMMARY ================================================================
 # This file deals with connecting to a database, performing data operations, 
 # and handling database-related exceptions.
+#  retrieve the menu (alcohol and not alcohol) with ingredients and allergies (Lydia)
+#  retrieve cocktail alcohol and non-alcohol main ingredients (Lydia)
+#  retrieve cocktail with a specific selected main ingredient (Lydia)
 # =========================================================================
 
 # Python Packages imports 
 import mysql.connector
 from config import HOST, USER, PASSWORD
-
 
 # Define a function to connect to the database with the given name
 def _connect_to_db(db_name):
@@ -24,125 +26,121 @@ def _connect_to_db(db_name):
 class DbConnectionError(Exception):
     pass
 
-# Define a function to get all cocktail recipes from the database
-def get_all_cocktail_recipes():
-    cocktail_menu = []  # Initialize an empty list to store cocktail data
+# Define a function to get the menu with ingredients and allergies
+
+# Example of using the get_menu function
+def get_menu():
+    menu = []
     try:
-        db_name = 'Cocktails'  # Name of the database to connect to
-        db_connection = _connect_to_db(db_name)  # Establish a database connection
-        cur = db_connection.cursor()  # Create a cursor object for executing SQL queries
-        print("Connected to DB: %s" % db_name)  # Print a message indicating a successful connection
+        db_name = 'Cocktails'  # Replace with your database name
+        db_connection = _connect_to_db(db_name)
+        cur = db_connection.cursor()
 
         query = """
-        SELECT *
-        FROM CocktailsInfo
+            SELECT
+                c.Id AS CocktailId,
+                c.CocktailName,
+                GROUP_CONCAT(
+                    CASE
+                        WHEN i.IsAlcoholic = TRUE THEN i.IngredientName
+                        ELSE NULL
+                    END
+                    SEPARATOR ', ') AS AlcoholicBeverage,
+                GROUP_CONCAT(
+                    CASE
+                        WHEN i.IsAlcoholic = FALSE THEN i.IngredientName
+                        ELSE NULL
+                    END
+                    SEPARATOR ', ') AS IngredientName
+            FROM CocktailsInfo c
+            LEFT JOIN CocktailIngredients ci ON c.Id = ci.CocktailId
+            LEFT JOIN Ingredients i ON ci.IngredientId = i.IngredientId
+            GROUP BY c.Id, c.CocktailName;
         """
 
-        cur.execute(query)  # Execute the SQL query to retrieve cocktail data
+        # Execute the query
+        cur.execute(query)
 
-        # Fetch all the data from the cursor and store it in cocktail_menu
-        cocktail_menu = cur.fetchall()
+        # Fetch the data
+        rows = cur.fetchall()
 
-        cur.close()  # Close the cursor
+        for row in rows:
+            cocktail_id, cocktail_name, alcoholic_beverage, ingredient_name = row
+            menu.append({
+                "CocktailId": cocktail_id,
+                "CocktailName": cocktail_name,
+                "AlcoholicBeverage": alcoholic_beverage,
+                "IngredientName": ingredient_name
+            })
 
-        # You can now work with the fetched data if needed
-        return cocktail_menu
-
-    except Exception:
-        raise DbConnectionError("Failed to read data from DB")  # Raise a custom exception for database connection errors
-
+    except Exception as e:
+        print("Failed to read data from DB:", str(e))
     finally:
         if db_connection:
-            db_connection.close()  # Close the database connection
-            print("DB connection is closed")  # Print a message indicating a closed connection
+            db_connection.close()
 
-def get_cocktail_by_id(cocktail_id):
-    try:
-        # Establish a database connection
-        db_connection = _connect_to_db('Cocktails')
-        cursor = db_connection.cursor()
+    return menu
 
-        # Execute an SQL query to fetch the cocktail by ID
-        query = "SELECT * FROM CocktailsInfo WHERE id = %s"
-        cursor.execute(query, (cocktail_id,))
+def get_alcoholic_ingredients(menu):
+    alcoholic_ingredients = set()  # Use a set to store unique values
 
+    for item in menu:
+        if item["AlcoholicBeverage"] is not None:
+            alcoholic_ingredients.add(item["AlcoholicBeverage"])
 
-        # Fetch the data
-        cocktail_data = cursor.fetchone()
+    return list(alcoholic_ingredients)  # Convert the set back to a list
 
-        # Close the cursor and the connection
-        cursor.close()
-        db_connection.close()
-       
-        return cocktail_data  # Return the cocktail data
-    except Exception as e:
-        raise DbConnectionError(f"Failed to fetch cocktail by ID: {str(e)}")
+def get_cocktail_by_alcoholic_beverage(menu, user_selection):
+    cocktails = []
+    for item in menu:
+        if item["AlcoholicBeverage"] is not None and user_selection in item["AlcoholicBeverage"]:
+            cocktails.append({
+                "CocktailId": item["CocktailId"],
+                "CocktailName": item["CocktailName"],
+                "AlcoholicBeverage": item["AlcoholicBeverage"],
+                "IngredientName": item["IngredientName"]
+            })
+    return cocktails
 
-
-def get_cocktails_by_ingredient(ingredient):
-    try:
-        cocktails_with_ingredient  = []  # Initialize an empty list to store cocktail data
-        # Establish a database connection
-        db_connection = _connect_to_db('Cocktails')
-        cursor = db_connection.cursor()
-
-        # Execute an SQL query to fetch cocktails by ingredient
-        query = "SELECT c.* FROM CocktailsInfo c JOIN CocktailIngredients ci ON c.Id = ci.CocktailId JOIN Ingredients i ON ci.IngredientId = i.IngredientId WHERE i.IngredientName LIKE %s"
-        cursor.execute(query, ('%'+ingredient+'%',))
-
-
-        # Fetch the data
-        cocktails_with_ingredient = cursor.fetchall()
-
-        # Close the cursor and the connection
-        cursor.close()
-        db_connection.close()
-        return cocktails_with_ingredient
-
-    except Exception as e:
-        raise DbConnectionError(f"Failed to fetch cocktails by ingredient: {str(e)}")
-
-def get_cocktails_sorted_by_name_asc():
-    try:
-        # Establish a database connection
-        db_connection = _connect_to_db('Cocktails')
-        cursor = db_connection.cursor()
-
-        # Execute an SQL query to fetch all cocktails and sort them by name in ascending order
-        query = "SELECT * FROM CocktailsInfo c ORDER BY c.CocktailName ASC"
-        cursor.execute(query)
-
-        # Fetch the data
-        sorted_cocktails = cursor.fetchall()
-
-        # Close the cursor and the connection
-        cursor.close()
-        db_connection.close()
-
-        return sorted_cocktails  # Return the sorted cocktails
-    except Exception as e:
-        raise DbConnectionError(f"Failed to fetch sorted cocktails: {str(e)}")
-
-
-# declaring cocktail variable to be populated based on table structure
-cocktail = {}
 
 
 # Define the main function, the entry point of the program
 def main():
-    # Connect to a database, but the database name is missing in the code
-    database = _connect_to_db('')  # we will need to add our database name
+    try:
+        # Connect to the 'Cocktails' database
+        database = _connect_to_db('Cocktails')
 
-    # Check if the database connection was successful
-    if database.is_connected():
-        # Uncomment the function call below to execute it (examples of database operations)
-        get_all_cocktail_recipes()
-    else:
-        # Print a message if the database connection failed
-        print("Database connection failed")
+        if database.is_connected():
+            menu = get_menu()  # Retrieve the menu
+            for item in menu:
+                print(
+                    f"CocktailId: {item['CocktailId']}, CocktailName: {item['CocktailName']}, AlcoholicBeverage: {item['AlcoholicBeverage']}, IngredientName: {item['IngredientName']}")
+
+            alcoholic_ingredients = get_alcoholic_ingredients(menu)  # Call the function with the menu
+            if alcoholic_ingredients:
+                print("Alcoholic beverages:")
+                for beverage in alcoholic_ingredients:
+                    print(f"Alcoholic Beverage: {beverage}")
+            else:
+                print("The Alcoholic beverages ingredients found in the menu are:")
+
+            user_selection = "White Rum"  # Replace with the user's selection
+            selected_cocktails = get_cocktail_by_alcoholic_beverage(menu, user_selection)
+
+            if selected_cocktails:
+                print(f"Cocktails with {user_selection}:")
+                for cocktail in selected_cocktails:
+                    print(
+                        f"CocktailId: {cocktail['CocktailId']}, CocktailName: {cocktail['CocktailName']}, AlcoholicBeverage: {cocktail['AlcoholicBeverage']}, IngredientName: {cocktail['IngredientName']}")
+            else:
+                print(f"No cocktails found with {user_selection}.")
+        else:
+            print("Database connection failed")
+
+    except Exception as e:
+        print("Failed to read data from DB:", str(e))
 
 
-# Check if this script is being run directly
 if __name__ == '__main__':
-    # Call the main function to start the program
     main()
+
